53d956a105035f2729ea94d275b21fd53f0aa946
Captain Fantasy <captain@fantasyui.com>
2017-11-27 11:57:34 -0500

    Automatic Musical Beat Generator

--- README.md
+++ README.md
@@ -1,14 +1,8 @@
 # shortwave
 Electron based music beat producer, powered by Node Emitter, Howler, Vue and Bootstrap.
 
-# Theory of Operation
+Simply Ctrl+R to regenerate beat. Interact with tiles via mouse clicks.
 
-A very simple musical instrument.
-
-Musical desktop with things that make a sounds aligned in a grid.
-
-Things that make a sound, are like broadcast stations that emit a customizable beep every X beats.
-
-A beat is a beat, standard musical BPM.
-
-The grid is a simple object storage device, it is the same kind of grid mode that you can put a standard Desktop with Icons into. It is just a way of keeping things tidy.
+![](screenshot-1.png)
+![](screenshot-2.png)
+![](screenshot-3.png)
--- renderer.js
+++ renderer.js
@@ -1,8 +1,132 @@
 
+const Wad = require('web-audio-daw');
+const Tuna = require('tunajs');
+
+Wad.tuna = new Tuna(Wad.audioContext);
+
 const EventEmitter = require('events');
 class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
 
+
+
+const wadPresets = [];
+
+wadPresets.push( new Wad({
+  source: 'sine',
+  vibrato : {  shape     : 'sine' },
+  env: {
+    attack: .001,
+    decay: .008,
+    sustain: .2,
+    hold: .03,
+    release: .01
+  },
+  filter: {
+    type: 'highpass',
+    frequency: 400,
+    q: 1
+  }
+}, ))
+
+
+
+
+
+wadPresets.push( new Wad({
+  source: 'sine',
+    vibrato : {  shape     : 'sine' },
+  env: {
+    attack: .001,
+    decay: .01,
+    sustain: .2,
+    hold: .03,
+    release: .02
+  },
+  filter: {
+    type: 'bandpass',
+    frequency: 300,
+    q: .180
+  }
+}, ));
+
+
+
+wadPresets.push( new Wad({
+  source: 'sine',
+    vibrato : {  shape     : 'sine' },
+  env: {
+    attack: .001,
+    decay: .008,
+    sustain: .2,
+    hold: .43,
+    release: .01
+  },
+  filter: {
+    type: 'highpass',
+    frequency: 100,
+    q: .2
+  }
+}, ));
+
+
+
+
+wadPresets.push( new Wad({
+  source: 'sine',
+    vibrato : {  shape     : 'sine' },
+  volume: .3,
+  env: {
+    attack: .01,
+    decay: .002,
+    sustain: .5,
+    hold: 2.5,
+    release: .3
+  },
+  filter: {
+    type: 'lowpass',
+    frequency: 600,
+    q: 7,
+    env: {
+      attack: .7,
+      frequency: 1600
+    }
+  },
+  vibrato: {
+    attack: 8,
+    speed: 8,
+    magnitude: 100
+  }
+}, ));
+
+
+wadPresets.push( new Wad({
+  source: 'sine',
+  vibrato : {  shape     : 'sine' },
+  env: {
+    attack: .001,
+    decay: .008,
+    sustain: .2,
+    hold: .03,
+    release: .01
+  },
+  filter: {
+    type: 'highpass',
+    frequency: 100,
+    q: 1
+  }
+}, ))
+
+
+
+
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
 
 
 new Vue({
@@ -10,25 +134,37 @@ new Vue({
 
   data: {
 
-    bpm: 256,
+    bpm: 320,
 
-    samples: {
+    states: [
+      {            rgba:[0,     0,  0,  0] },
 
-      silence: { sound:'', icon:'' },
-         slam: { sound:'slam.wav', icon:'heart' },
-       crunch: { sound:'crunch.wav', icon:'th' },
+      { preset:0, rgba:[193,  63,  33, 1] },
+      { preset:1, rgba:[210, 110,  45, 1] },
+      { preset:2, rgba:[221, 160,  50, 1] },
+      { preset:3,  rgba:[120, 175, 159, 1] },
+      { preset:4,  rgba:[101, 156, 200, 1] },
 
-    },
+    ],
 
     cells: [
-      [{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},],
-      [{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},],
-      [{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},],
-      [{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},],
-      [{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},],
-      [{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},],
-      [{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},],
-      [{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},{active:false, device:null},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+      [{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},{active:false, state:0},],
+
 
     ],
 
@@ -36,57 +172,84 @@ new Vue({
 
   created: function () {
 
-    myEmitter.on('cell', (coordinates) => {
-      //console.log('coordinates',coordinates);
+    myEmitter.on('cell', (cell) => {
+      const state = this.states[cell.state];
+      console.log( cell );
 
-      if(coordinates.sound){
-
-        let soundData =  this.sounds[ coordinates.sound ];
-
-        var sound = new Howl({
-          src: [soundData.sound]
-        });
-
-      sound.play();
+      if(cell.pitch){
+        wadPresets[state.preset].play({pitch : cell.pitch})
       }
+
 
     })
 
+    for(let x = 0; x<this.cells[0].length;x++){
+      this.cells[0][x].active = true;
+    }
     for(let y = 1; y<this.cells.length;y++){
       for(let x = 0; x<this.cells[y].length;x++){
         this.cells[y][x].active = false;
       }
     }
 
+
+    for(let y = 0; y<this.cells.length;y++){
+      const yp = parseFloat(((y * 100 / (this.cells.length-1))/100).toFixed(2));
+      for(let x = 0; x<this.cells[y].length;x++){
+        const xp = parseFloat(((x * 100 / (this.cells[y].length-1))/100).toFixed(2));
+        this.cells[y][x].yp = yp;
+        this.cells[y][x].xp = xp;
+
+        this.cells[y][x].pitch = (2000*xp)+100;
+
+      }
+    }
+
+    /// Make a song
+    for(let y = 1; y<this.cells.length;y++){
+      for(let x = 2; x<this.cells[y].length;x++){
+        if (!getRandomInt(0,24)) this.cells[y][x].state = getRandomInt(0,this.states.length)
+      }
+    }
+
+    /// Make a beat
+    const bum1 = getRandomInt(1,this.states.length);
+    const bum2 = getRandomInt(1,this.states.length);
+    const skip = getRandomInt(2,4);
+
+    for(let y = 1; y<this.cells.length;y=y+skip){
+
+      this.cells[y][0].state = bum1;
+      this.cells[y-1][1].state = bum2;
+
+    }
+
+
     let y = this.cells.length-2;
     const payload = ()=>{
-      //console.log('TICK')
       y++;
       if(y>=this.cells.length) y = 0 ;
-
         for(let x = 0; x<this.cells[y].length;x++){
-        //console.log("/",y,x)
         if(this.cells[y][x].active ){
-
           let nextActiveColX = x;
           let nextActiveColY = y+1;
           if(nextActiveColY>=this.cells.length) nextActiveColY = 0;
-
           this.cells[y][x].active = false;
           this.cells[nextActiveColY][nextActiveColX].active = true;
-          myEmitter.emit('cell', Object.assign({x:nextActiveColX, y:nextActiveColY},this.cells[nextActiveColY][nextActiveColX]));
+
+          if(this.cells[nextActiveColY][nextActiveColX].state) myEmitter.emit('cell', Object.assign({x:nextActiveColX, y:nextActiveColY},this.cells[nextActiveColY][nextActiveColX]));
+
         }
       }
     }
-    //setInterval(payload, (1000*60)/this.bpm);
+    setInterval(payload, (1000*60)/this.bpm);
     payload();
-
   },
 
   computed: {
 
     cellWidth:function(){return 100/this.cells[0].length},
-    cellHeight:function(){return 100/this.cells.length},
+    cellHeight:function(){return 95/this.cells.length},
 
 
   },
@@ -95,8 +258,17 @@ new Vue({
 
     setStation:function(cell){
 
-      console.log(cell);
-      cell.active = true;
+      cell.state++;
+      const selected = this.states[cell.state];
+      console.log(`rgb(${selected.rgba.slice(0,3).map(i=> i * .8|1 ).join()})` )
+
+      if(cell.state>(this.states.length-2)) cell.state = 0;
+
+      if (cell.state === 0) {
+        cell.active = false;
+      } else {
+        cell.active = true;
+      }
 
     },
 
@@ -129,13 +301,26 @@ new Vue({
 
     cellIcon:function(cell){
       if (cell.sound) return this.sounds[cell.sound].icon;
-
     },
+
     cellClass:function(cell){
-      if(cell.active&&cell.sound) return 'cell-active cell-sound';
       if(cell.active) return 'cell-active';
       if(cell.sound) return 'cell-sound';
-      return 'bg';
+      //return 'bg';
+    },
+
+    cellStyle:function(cell){
+
+      if(!cell.state) return;
+
+      const selected = this.states[cell.state];
+
+      if(!selected) return;
+      return {
+        backgroundColor: `rgba(${selected.rgba.join()})`,
+        borderColor: `rgb(${selected.rgba.slice(0,3).map(i=> i * .8|1 ).join()})`,
+      }
+
     },
 
 
@@ -143,3 +328,10 @@ new Vue({
   }
 
 })
+
+
+// rgb(193, 63, 33)
+// rgb(210, 110, 45)
+// rgb(221, 160, 50)
+// rgb(120, 175, 159)
+// rgb(101, 156, 200)
